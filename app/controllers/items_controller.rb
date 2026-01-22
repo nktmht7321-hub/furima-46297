614db98970ad21e8c1d5e983524383ed5e57b635
nktmht7321-hub <nktmht7321@gmail.com>
2026-01-22 18:36:45 +0900

    ルーティングとコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,30 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    # 保存できた場合とできなかった場合で条件分岐
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image, :name, :info, :price,
+      :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id
+    ).merge(user_id: current_user.id)
   end
 end
