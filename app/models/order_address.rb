65d2d617d739417c1a7126a99132aa76f9437b39
nktmht7321-hub <nktmht7321@gmail.com>
2026-01-23 22:16:33 +0900

    テストコード作成

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,15 +5,18 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: {with: /\A[4-12]{3}-[4-12]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
-    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :house_number
-    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
+
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
   end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number,
+                   building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
---
+++ spec/factories/order_address_spec.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code { '123-4567' }
+    prefecture_id { 2 }
+    city { '東京都' }
+    house_number { '1-1' }
+    building_name { '東京ハイツ' }
+    phone_number { '09012345678' }
+    # token { "tok_abcdefghijk00000000000000000" } # STEP2で追加します
+
+    user_id { FactoryBot.create(:user).id }
+    item_id { FactoryBot.create(:item).id }
+  end
+end
